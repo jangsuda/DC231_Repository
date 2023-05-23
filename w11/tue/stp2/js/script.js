24f94de742aa9270356c1138fe1aef20eff0a057
jangsuda <126849533+jangsuda@users.noreply.github.com>
2023-05-24 00:42:55 +0900

    w11 수업

---
+++ w11/tue/stp2/js/script.js
@@ -0,0 +1,17 @@
+let aCard = document.querySelector(".card");
+console.log(aCard);
+aCard.onclick = (e) => {
+  console.log("target", e.target);
+  console.log("currentTarget", e.currentTarget);
+  e.currentTarget.classList.toggle(
+    "card-selected"
+  );
+};
+
+// let elem = document.getElementById("card1");
+// elem.onclick = () => {
+//   elem.classList.toggle("card-selected");
+// };
+
+// elem.onclick = () => {}랑
+// elem.onclick = function () {} 같은 말.
