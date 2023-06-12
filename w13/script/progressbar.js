045bcadd99384b51005cd04d6558842f08afb8fa
jangsuda <126849533+jangsuda@users.noreply.github.com>
2023-06-13 04:56:28 +0900

    w13주차 stp3

---
+++ w13/script/progressbar.js
@@ -0,0 +1,19 @@
+const indicator = document.querySelector(".indicator");
+const progressTarget = document.querySelector("main");
+
+const progressBar = () => {
+  const progressTargetBCR = progressTarget.getBoundingClientRect();
+  //   console.log(progressTargetBCR);
+  const progress = 1 - progressTargetBCR.bottom / progressTargetBCR.height;
+  console.log(progress);
+  if (progress >= 0 && progress <= 1) {
+    indicator.style.cssText = "transform: scaleX(" + progress + ")";
+  }
+};
+
+// // 이벤트 리스너 추가하는 법 1
+// document.onscroll = () => {
+//   progressBar();
+// };
+// 이벤트 리스너 추가하는 법 2
+document.addEventListener("scroll", progressBar);
